8564ff96858cd4682bc7fc44eb5ae8166f493c12
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-28 21:40:41 +0700

    Add wrapper to Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { Link, useRouteError } from "react-router-dom";
 import img from "../assets/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
   const error = useRouteError();
   if (error.status === 404) {
     return (
-      <div>
+      <Wrapper>
         <div>
           <img src={img} alt="not found" className="img" />
           <h3>Ohh! </h3>
           <p>We can't seem to find the page you're looking for</p>
           <Link to="/">back home</Link>
         </div>
-      </div>
+      </Wrapper>
     );
   }
 
   return (
-    <div>
+    <Wrapper>
       <div>
         <h3>something went wrong</h3>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
