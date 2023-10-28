fd97f7673d53761c1629fb1d3184485a44a9a06a
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-28 18:19:18 +0700

    Customize Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>nav</div>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <span className="logo">MixMaster</span>
+        <div className="nav-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+          <Link to="/newsletter" className="nav-link">
+            Newsletter
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
