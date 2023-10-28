beaeb6b49b73670e57b5fbb8752abf8ea12f90f1
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-28 18:19:05 +0700

    Create navbar component and add to HomeLayout

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const HomeLayout = () => {
   return (
     <div>
-      <nav>navbar</nav>
+      <Navbar />
       <Outlet />
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return <div>nav</div>;
+};
+
+export default Navbar;
