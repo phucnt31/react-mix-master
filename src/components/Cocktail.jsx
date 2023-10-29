18662d8c50f8fad86850582dab1707bffc91576b
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-29 20:20:55 +0700

    Render info in Cocktail.jsx

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -13,8 +13,52 @@ export const loader = async ({ params }) => {
 
 const Cocktail = () => {
   const { data } = useLoaderData();
-  console.log(data);
-  return <div>Cocktail</div>;
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: img,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  return (
+    <div>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={img} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">Name : </span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">Category : </span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">Info : </span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">Glass : </span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">Instructions : </span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cocktail;
