c4730b9a1b1ed792347380db45682cbda2927257
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-29 20:43:46 +0700

    Customize Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,7 +1,59 @@
 import React from "react";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-input"
+          defaultValue="john"
+        />
+      </div>
+      {/* lastName */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          lastName
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="form-input"
+          defaultValue="smith"
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          className="form-input"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default Newsletter;
