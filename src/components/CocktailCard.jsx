c77d60513e275e45db693baf57d138f03e2c5ab1
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-29 19:48:07 +0700

    Customize CocktailCard.jsx

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const CocktailCard = () => {
-  return <div>CocktailCard</div>;
+const CocktailCard = ({ id, img, name, info, glass }) => {
+  return (
+    <article>
+      <img src={img} alt={name} className="img" />
+      <div className="footer">
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+      </div>
+    </article>
+  );
 };
 
 export default CocktailCard;
