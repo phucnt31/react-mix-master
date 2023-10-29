37fab7ce6ae174a8eb098b9372e3dc420b4c47af
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-29 19:25:36 +0700

    Setup fetch drinks with search term

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import axios from "axios";
+
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export const loader = async () => {
-  return "something";
+  const searchTerm = "margarita";
+  const resp = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  console.log(resp);
+  return { drinks: resp.data.drinks, searchTerm };
 };
 
 const Landing = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return <div>Landing</div>;
 };
 
