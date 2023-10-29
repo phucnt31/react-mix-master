6726ce53f129699f62a00d40a19c8a18ee6fb189
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-29 19:49:14 +0700

    Add wrapper to CocktailCard.jsx

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/CocktailCard";
 
 const CocktailCard = ({ id, img, name, info, glass }) => {
   return (
-    <article>
+    <Wrapper>
       <img src={img} alt={name} className="img" />
       <div className="footer">
         <h4>{name}</h4>
         <h5>{glass}</h5>
         <p>{info}</p>
       </div>
-    </article>
+    </Wrapper>
   );
 };
 
