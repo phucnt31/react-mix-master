f1d1ff7739c6ba2d774c5f1c583019e12d9b9c1c
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-29 19:43:45 +0700

    Customize CocktailList.jsx

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import CocktailCard from "./CocktailCard";
 
-const CocktailList = () => {
-  return <div>CocktailList</div>;
+const CocktailList = ({ drinks }) => {
+  if (!drinks) {
+    return (
+      <h4 style={{ textAlign: "center" }}>No matching cocktails found...</h4>
+    );
+  }
+
+  const formattedDrinks = drinks.map((drink) => {
+    const { idDrink, strDrinkThumb, strGlass, strDrink, strAlcoholic } = drink;
+    return {
+      id: idDrink,
+      img: strDrinkThumb,
+      glass: strGlass,
+      name: strDrink,
+      info: strAlcoholic,
+    };
+  });
+
+  return (
+    <div>
+      {formattedDrinks.map((drink) => {
+        return <CocktailCard key={drink.id} {...drink} />;
+      })}
+    </div>
+  );
 };
 
 export default CocktailList;
