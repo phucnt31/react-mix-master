42f71ef89074980ac4f857ce0ed27e82ce5b11b7
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-30 17:54:55 +0700

    Change folder name

--- src/components/SinglePageError.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useRouteError } from "react-router-dom";
-
-const SinglePageError = () => {
-  const error = useRouteError();
-  return <div>{error.message}</div>;
-};
-
-export default SinglePageError;
