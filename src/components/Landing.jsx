596f3c0d7e5aa100fcd3e1ae176c995d55b667b3
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-30 16:50:59 +0700

    Search functionality

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,8 +7,9 @@ import CocktailList from "./CocktailList";
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-export const loader = async () => {
-  const searchTerm = "margarita";
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchTerm = url.searchParams.get("search") || "";
   const resp = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
   return { drinks: resp.data.drinks, searchTerm };
 };
@@ -18,7 +19,7 @@ const Landing = () => {
 
   return (
     <>
-      <SearchForm />
+      <SearchForm searchTerm={searchTerm} />
       <CocktailList drinks={drinks} />
     </>
   );
