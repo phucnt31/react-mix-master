422cff472df1b6b637c6c0178a6ffb6d31981b14
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-30 17:32:56 +0700

    Setup react-query for Landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,20 +3,32 @@ import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 import SearchForm from "./SearchForm";
 import CocktailList from "./CocktailList";
+import { useQuery } from "@tanstack/react-query";
 
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
+const searchCocktails = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const resp = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return resp.data.drinks;
+    },
+  };
+};
+
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get("search") || "";
-  const resp = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return { drinks: resp.data.drinks, searchTerm };
+
+  return { searchTerm };
 };
 
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
-
+  const { searchTerm } = useLoaderData();
+  const { data: drinks, isLoading } = useQuery(searchCocktails(searchTerm));
+  console.log(isLoading);
   return (
     <>
       <SearchForm searchTerm={searchTerm} />
