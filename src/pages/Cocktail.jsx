bf370156c2ff5744778bf9873135bf4bfbc245d3
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-30 18:02:29 +0700

    Refactor to new folder

---
+++ src/pages/Cocktail.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useLoaderData, Link, Navigate } from "react-router-dom";
+import axios from "axios";
+import Wrapper from "../assets/wrappers/CocktailPage";
+import { useQuery } from "@tanstack/react-query";
+
+const singleCocktailUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+const singleCocktail = (id) => {
+  return {
+    queryKey: ["cocktail", id],
+    queryFn: async () => {
+      const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+      return data;
+    },
+  };
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const { id } = params;
+    await queryClient.ensureQueryData(singleCocktail(id));
+    return { id };
+  };
+
+const Cocktail = () => {
+  const { id } = useLoaderData();
+  const { data } = useQuery(singleCocktail(id));
+
+  if (!data.drinks) {
+    return <Navigate to="/" />;
+  }
+
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: img,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  const ingredients = Object.keys(singleDrink)
+    .filter(
+      (item) => item.startsWith("strIngredient") && singleDrink[item] !== null
+    )
+    .map((item) => singleDrink[item]);
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={img} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">Name : </span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">Category : </span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">Info : </span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">Ingredients : </span>
+            {ingredients.map((item, index) => {
+              return (
+                <span className="ing" key={index}>
+                  {item}
+                  {index < ingredients.length - 1 ? ", " : ""}
+                </span>
+              );
+            })}
+          </p>
+          <p>
+            <span className="drink-data">Glass : </span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">Instructions : </span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Cocktail;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import axios from "axios";
+import SearchForm from "./SearchForm";
+import CocktailList from "./CocktailList";
+import { useQuery } from "@tanstack/react-query";
+
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+const searchCocktails = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const resp = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return resp.data.drinks;
+    },
+  };
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get("search") || "";
+    await queryClient.ensureQueryData(searchCocktails(searchTerm));
+    return { searchTerm };
+  };
+
+const Landing = () => {
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktails(searchTerm));
+
+  return (
+    <>
+      <SearchForm searchTerm={searchTerm} />
+      <CocktailList drinks={drinks} />
+    </>
+  );
+};
+
+export default Landing;
