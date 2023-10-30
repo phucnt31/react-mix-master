8830c20cd60d23d38a4daac1bf590a41f378b7e4
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-30 16:38:36 +0700

    Customize SearchForm.jsx

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  return (
+    <div>
+      <form className="form">
+        <input
+          type="text"
+          name="search"
+          className="form-input"
+          defaultValue="vodka"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchForm;
